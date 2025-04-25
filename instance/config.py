ac83b4a527f1c353b04b62138e39882f4713db15
lazarovph <90379778+lazarovph@users.noreply.github.com>
2025-04-25 17:08:35 +0300

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,15 +1,15 @@
 import os
 
-# Секретен ключ за сесии и сигурност
+# SECRET KEY
 SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
 
-# Конфигурация за базата данни (SQLite в случая)
+# config DB SQLite
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Настройки за логване
+# logging
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'DEBUG')
 
-# Други конфигурации (по избор)
+# other
 # EMAIL_USER = os.environ.get('EMAIL_USER')
 # EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
